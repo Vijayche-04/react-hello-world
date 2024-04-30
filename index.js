d7940a1772e637406933be5de7edda6947042d85
vijay che <vijaykumar.java04@gmail.com>
2024-04-30 14:29:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const parent = React.createElement(
+    "div",
+     {id: "parent"},
+     React.createElement(
+        "div",
+        {id: "child"},
+        [
+            React.createElement(
+                "h1",
+                {},
+                "H1 Tag From React"
+            ),
+            React.createElement(
+                "h2",
+                {},
+                "H2 tag from React"
+            )
+
+        ]
+     )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
